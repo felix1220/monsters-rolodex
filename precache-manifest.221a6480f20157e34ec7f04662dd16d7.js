bd7931981090cdc4255d504d0492b33082147715
Ruisanchez <FRuisanchez@srtrl.com>
2019-10-10 09:10:20 -0400

    Updates

---
+++ precache-manifest.221a6480f20157e34ec7f04662dd16d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a55f5ddc77886cfaa470b97885ee8376",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a4ac47319b88441c0a0f",
+    "url": "/monsters-rolodex/static/css/main.f251c76e.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/monsters-rolodex/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "a4ac47319b88441c0a0f",
+    "url": "/monsters-rolodex/static/js/main.fcdc3816.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
